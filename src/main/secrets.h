d4b46daee5e6083bc9d7a20b5666961c9b9d79f3
Pascal Hartig <phartig@rdrei.net>
2019-03-03 22:24:01 +0000

    Initial commit

---
+++ src/main/secrets.h
@@ -0,0 +1,11 @@
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       ""
+#define WLAN_PASS       ""
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    ""
+#define AIO_KEY         ""
