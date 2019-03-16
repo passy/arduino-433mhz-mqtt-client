b6522613f76bbdcc27f114c49f2d5e34c082ccf4
Pascal Hartig <phartig@rdrei.net>
2019-03-16 12:36:18 +0000

    Clean up

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,58 +1,44 @@
-/***************************************************
-  Adafruit MQTT Library ESP8266 Example
-  Must use ESP8266 Arduino from:
-    https://github.com/esp8266/Arduino
-  Works great with Adafruit's Huzzah ESP board & Feather
-  ----> https://www.adafruit.com/product/2471
-  ----> https://www.adafruit.com/products/2821
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-  Written by Tony DiCola for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
-#include <ESP8266WiFi.h>
-#include <RCSwitch.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 #include "secrets.h"
+#include <ESP8266WiFi.h>
+#include <RCSwitch.h>
 
-// the on off button feed turns this LED on/off
-#define LED 2  
-// the slider feed sets the PWM output of this pin
-#define PWMOUT 12
+/************ Constants ************/
+
+#define LED 2
 
 /************ Global State ******************/
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
+// You should absolutely use the WiFiClientSecure here if your
+// board supports it.
 WiFiClient client;
-// or... use WiFiFlientSecure for SSL
-//WiFiClientSecure client;
 
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and
+// login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME,
+                          AIO_USERNAME, AIO_KEY);
 
 RCSwitch sender = RCSwitch();
 
 /****************************** Feeds ***************************************/
 
 // Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
-Adafruit_MQTT_Subscribe slider = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/slider");
-Adafruit_MQTT_Subscribe zap = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/zap");
-
-/*************************** Sketch Code ************************************/
-
-// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
-// for some reason (only affects ESP8266, likely an arduino-builder bug).
-void MQTT_connect();
+Adafruit_MQTT_Subscribe zap =
+    Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/zap");
 
 void setup() {
   pinMode(LED, OUTPUT);
-  pinMode(PWMOUT, OUTPUT);
 
   Serial.begin(115200);
-  sender.enableTransmit(16); // I hope this maps to D0.
+
+  // Turn LED off until MQTT is connected.
+  digitalWrite(LED, HIGH);
+
+  // Check what the pin mapping for your board is.
+  // For mine this is D0.
+  sender.enableTransmit(16);
   sender.setProtocol(1);
   sender.setPulseLength(187);
 
@@ -69,20 +55,17 @@ void setup() {
   Serial.println();
 
   Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
   // Setup MQTT subscription for onoff & slider feed.
-  mqtt.subscribe(&onoffbutton);
-  mqtt.subscribe(&slider);
   mqtt.subscribe(&zap);
 }
 
-uint32_t x=0;
-
 void loop() {
   // Ensure the connection to the MQTT server is alive (this will make the first
-  // connection and automatically reconnect when disconnected).  See the MQTT_connect
-  // function definition further below.
+  // connection and automatically reconnect when disconnected).  See the
+  // MQTT_connect function definition further below.
   MQTT_connect();
 
   // this is our 'wait for incoming subscription packets' busy subloop
@@ -90,43 +73,19 @@ void loop() {
 
   Adafruit_MQTT_Subscribe *subscription;
   while ((subscription = mqtt.readSubscription(5000))) {
-    // Check if its the onoff button feed
-    if (subscription == &onoffbutton) {
-      Serial.print(F("On-Off button: "));
-      Serial.println((char *)onoffbutton.lastread);
-      
-      if (strcmp((char *)onoffbutton.lastread, "ON") == 0) {
-        sender.sendTriState("FFF0FFFF1001");
-        digitalWrite(LED, LOW); 
-      }
-      if (strcmp((char *)onoffbutton.lastread, "OFF") == 0) {
-        sender.sendTriState("FFF0FFFF1010");
-        digitalWrite(LED, HIGH); 
-      }
-    }
-
     if (subscription == &zap) {
       Serial.print(F("Zap: "));
-      const char* val = (char *) zap.lastread;
+      const char *val = (char *)zap.lastread;
       Serial.println(val);
 
       sender.sendTriState(val);
     }
-    
-    // check if its the slider feed
-    if (subscription == &slider) {
-      Serial.print(F("Slider: "));
-      Serial.println((char *)slider.lastread);
-      uint16_t sliderval = atoi((char *)slider.lastread);  // convert to a number
-      analogWrite(PWMOUT, sliderval);
-    }
   }
 
   // ping the server to keep the mqtt connection alive
-  if(! mqtt.ping()) {
+  if (!mqtt.ping()) {
     mqtt.disconnect();
   }
-
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
@@ -142,16 +101,20 @@ void MQTT_connect() {
   Serial.print("Connecting to MQTT... ");
 
   uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
+  // connect will return 0 for connected
+  while ((ret = mqtt.connect()) != 0) {
+    digitalWrite(LED, HIGH);
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000); // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1)
+        ;
+    }
   }
+  digitalWrite(LED, LOW);
   Serial.println("MQTT Connected!");
 }
